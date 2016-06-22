56b2f2cf3ae933849545f218a183459166acf104
Minister Face <ministerface@gmail.com>
2016-06-22 15:10:07 +0300

    add form to addProject component

--- src/components/project/AddProject.js
+++ src/components/project/AddProject.js
@@ -26,22 +26,69 @@ export default class AddProject extends Component {
              <Modal.Title>Новый проект</Modal.Title>
            </Modal.Header>
            <Modal.Body>
-             <h4>Text in a modal</h4>
-             <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+             <form className="form-horizontal">
+               <div className="form-group">
+                 <label className="col-md-2 control-label">Название</label>
+                 <div className="col-md-10">
+                   <input type="text" className="form-control" value="" />
+                 </div>
+               </div>
+
+               <div className="form-group">
+                 <label className="col-md-2 control-label">Описание</label>
+                 <div className="col-md-10">
+                   <textarea className="form-control" rows="2"></textarea>
+                 </div>
+               </div>
+
+               <div className="form-group">
+                 <label className="col-md-2 control-label">Бюджет</label>
+                 <div className="col-md-10">
+                   <input type="text" className="form-control" placeholder="" />
+                 </div>
+               </div>
+               <div className="form-group">
+                 <label className="col-md-2 control-label">Сроки </label>
+                 <div className="col-md-10">
+                   <input type="text" className="form-control" placeholder="" />
+                 </div>
+               </div>
+
+               <div className="form-group">
+                 <label className="col-md-2 control-label">Заказчик</label>
+                 <div className="col-md-10">
+                   <input type="text" className="form-control" value="" />
+                 </div>
+               </div>
+
+               <div className="form-group">
+                 <label className="col-md-2 control-label">Телефон</label>
+                 <div className="col-md-10">
+                   <input type="text" className="form-control" value="" />
+                 </div>
+               </div>
 
 
-             <hr />
+               <div className="form-group">
+                 <label className="col-md-2 control-label" for="example-email">Email</label>
+                 <div className="col-md-10">
+                   <input type="email" id="example-email" name="example-email" className="form-control" placeholder="" />
+                 </div>
+               </div>
 
-             <h4>Overflowing text to show scroll behavior</h4>
-             <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-             <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-             <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
-             <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-             <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-             <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+               <div className="form-group">
+                 <label className="col-md-2 control-label">Город</label>
+                 <div className="col-md-10">
+                   <input type="text" className="form-control" value="" />
+                 </div>
+               </div>
+
+
+
+             </form>
            </Modal.Body>
            <Modal.Footer>
-             <Button onClick={::this.closeModal}>Close</Button>
+             <Button onClick={::this.closeModal}>Отмена</Button>
            </Modal.Footer>
          </Modal>
 
